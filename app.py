a0a5be6f6f362ba9d717de643179e8b43a48013c
YounusVersiani <flashyintel@gmail.com>
2025-11-29 01:15:55 +0300

    Modified app.py for correct imports.

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain 
 from langchain_core.prompts import ChatPromptTemplate
+
 
 # Load environment variables
 load_dotenv()
